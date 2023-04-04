c261cbdf5c3dc7e9d8e56ad17473b3f0a7ca1378
pavangithub630 <pitcheripavan@gmail.com>
2023-04-04 10:43:35 +0530

    initial commit

---
+++ jobBoardBackend/src/main/java/com/pavan/jobBoardBackend/JobBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.jobBoardBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardBackendApplication.class, args);
+	}
+
+}
